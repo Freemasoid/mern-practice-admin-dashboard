2433eae881e6b8bcfaeb8e996d014a854a31abb8
Freemasoid <freemasoid@icloud.com>
2024-05-31 13:30:44 +0200

    fix: call to store to get theme data

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useSelector } from "react-redux";
 import { darkTheme, lightTheme } from "./muiThemes.js";
 import { useMemo } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { SharedLayout, Dashboard } from "./pages";
 
 function App() {
-  const mode = useSelector((state) => state.globalTheme.mode);
+  const mode = useSelector((store) => store.theme.mode);
   const theme = useMemo(() => (mode === "dark" ? darkTheme : lightTheme), [mode]);
 
   return (
-    <>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
       </ThemeProvider>
-    </>
+      <Routes>
+        <Route element={<SharedLayout />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
