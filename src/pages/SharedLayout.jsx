cb48bd43e0dea404be285b19c59c89e1b71c4270
Freemasoid <freemasoid@icloud.com>
2024-06-12 19:09:33 +0200

    feat: get user and pass it so sidebar and navbar

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -3,20 +3,29 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Navbar, Sidebar } from "../components";
+import { useGetUserQuery } from "../../utils/api.js";
 
 function SharedLayout() {
   const isMobile = useMediaQuery("(max-width: 37.5rem)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((store) => store.state.userId);
+  const { data } = useGetUserQuery(userId);
+
   return (
     <Box display={isMobile ? "block" : "flex"} width="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isMobile={isMobile}
         drawerWidth="15.625rem"
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
       <Box>
-        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+        <Navbar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <span>{`isMobile = ${isMobile}`}</span>
         <br />
         <span>{`Display property = ${isMobile ? "block" : "flex"}`}</span>
