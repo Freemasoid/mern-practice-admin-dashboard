68f61b704ae97508d01487e57aaff8bb3c4a79b6
Freemasoid <freemasoid@icloud.com>
2024-06-12 17:45:04 +0200

    feat: setup createApi

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./features/theme/themeSlice.js";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "../utils/api.js";
 
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
+
+setupListeners(store.dispatch);
---
+++ utils/api.js
@@ -0,0 +1,72 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  // eslint-disable-next-line no-undef
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+    "Admin",
+    "Performance",
+    "Dashboard",
+  ],
+  endpoints: (builder) => ({
+    getUser: builder.query({
+      query: (id) => `main/user${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: builder.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: builder.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+    getTransactions: builder.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: { page, pageSize, sort, search },
+      }),
+      providesTags: ["Transactions"],
+    }),
+    getGeography: builder.query({
+      query: () => "client/geography",
+      providesTags: ["Geography"],
+    }),
+    getSales: builder.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+    getAdmin: builder.query({
+      query: () => "control/admin",
+      providesTags: ["Admin"],
+    }),
+    getUserPerformance: builder.query({
+      query: (id) => `control/performance/${id}`,
+      providesTags: ["Performance"],
+    }),
+    getDashboard: builder.query({
+      query: () => "main/dashboard",
+      providesTags: ["Dashboard"],
+    }),
+  }),
+});
+
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
+  useGetAdminQuery,
+  useGetUserPerformanceQuery,
+  useGetDashboardQuery,
+} = api;
